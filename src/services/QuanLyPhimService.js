35713f635330a24abdb49e6387627fa58e5919a4
Hoang Phi <philehoang99@gmail.com>
2021-07-14 16:16:23 +0700

    moviebooking

---
+++ src/services/QuanLyPhimService.js
@@ -0,0 +1,24 @@
+import { baseService } from "./baseService";
+import { GROUPID} from '../util/settings/config'
+export class QuanLyPhimService  extends baseService{
+
+    constructor() {
+        super();
+    }
+
+    layDanhSachBanner = () => {
+        return this.get(`/api/QuanLyPhim/LayDanhSachBanner`);
+    }
+    
+    layDanhSachPhim = () => {
+        return this.get(`/api/QuanLyPhim/LayDanhSachPhim?maNhom=${GROUPID}`)
+    }
+
+    layThongTinPhim = (maPhim) => {
+        return this.get(`/api/QuanLyPhim/LayThongTinPhim?MaPhim=${maPhim}`)
+    }
+}
+
+
+
+export const quanLyPhimService = new QuanLyPhimService();
